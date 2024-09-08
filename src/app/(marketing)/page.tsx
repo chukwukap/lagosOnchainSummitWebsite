09f946f6de5ec734aed4198d405d98e3d670b6e3
Chukwuka Uba <promiseuba67@gmail.com>
2024-09-08 02:38:35 -0700

    update the marketing page. comment contact form section

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="">
       <HeroSection />
-      <ContestForm />
+      {/* <ContestForm /> */}
       <SponsorsSection />
       <AboutSection />
       <ContestSection />
